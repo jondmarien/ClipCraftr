9c31c2037538575994ca53dfb455724b34ec6a0e
jondmarien <jon@chron0.tech>
2025-06-01 19:11:36 -0400

    chore(dash): updated fonts, added favicon icons

--- packages/dashboard/tailwind.config.js
+++ packages/dashboard/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-aeonik)', 'sans-serif'],
+      },
       colors: {
         primary: {
           50: '#f0f9ff',
