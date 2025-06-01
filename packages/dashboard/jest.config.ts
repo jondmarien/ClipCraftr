90a6e04d8ca35b185bf11a7ef5aa7f169ee464cc
jondmarien <jon@chron0.tech>
2025-06-01 04:11:18 -0400

    chore(dash): used proper `jest` config file

---
+++ packages/dashboard/jest.config.ts
@@ -0,0 +1,44 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['@swc/jest', { sourceMaps: true }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(node-fetch|fetch-blob|node:.*|@babel/runtime/helpers/esm/)).*$',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!.next/**',
+    '!**/types/**',
+    '!**/styles/**',
+    '!**/constants/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/cypress/'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+};
+
+export default createJestConfig(config);
